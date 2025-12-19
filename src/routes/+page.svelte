c2b619aa63b6966bce18fc3ae8ed48686f8038b5
Andy Pan <andy0130tw@yahoo.com.tw>
2025-12-20 04:20:35 +0800

    Make the first step

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,85 @@
 <script>
-  let counter = 0
+  import { basicSetup } from 'codemirror'
+  import { EditorView, ViewPlugin } from '@codemirror/view'
+  import { Compartment } from '@codemirror/state'
+
+  /** @type {EditorView} */
+  let cmView
+
+  /**
+   * @template {Node} T
+   * @param {T} el
+   * @param {(el: T) => boolean} queryIsDark
+   * @returns {import('@codemirror/state').Extension}
+   **/
+  function cmColorModePlugin(el, queryIsDark) {
+    const compartment = new Compartment()
+
+    const initialIsDark = queryIsDark(el)
+    const colorExt = (/** @type {boolean} */dark) => dark ? EditorView.darkTheme.of(true) : []
+
+    const plugin = ViewPlugin.fromClass(class {
+      /** @param {EditorView} view */
+      constructor(view) {
+        this.view = view
+        this.isDark = initialIsDark
+        this.watcher = this.startWatch()
+      }
+
+      startWatch() {
+        const obs = new MutationObserver(() => {
+          const isDark = queryIsDark(el)
+          if (this.isDark != isDark) {
+            this.updateThemeConfig(isDark)
+          }
+        })
+        obs.observe(el, { attributes: true, attributeFilter: ['class'] })
+        return obs
+      }
+
+      /** @param {boolean} dark */
+      updateThemeConfig(dark) {
+        this.view.dispatch({
+          effects: compartment.reconfigure(colorExt(dark)),
+        })
+        this.isDark = dark
+      }
+
+      destroy() {
+        this.watcher.disconnect()
+      }
+    })
+
+    return [plugin, compartment.of(colorExt(initialIsDark))]
+  }
+
+  /** @type {import('svelte/attachments').Attachment} */
+  function cm(dom) {
+    cmView = new EditorView({
+      doc: 'lorem\n',
+      extensions: [
+        basicSetup,
+        EditorView.baseTheme({
+          '&': {
+            border: '1px solid #ddd',
+            height: '100dvh',
+          },
+          '&dark': {
+            border: '1px solid #333338',
+          },
+        }),
+        cmColorModePlugin(
+          dom.ownerDocument.documentElement,
+          (el) => el.classList.contains('quiet-dark')),
+      ],
+      parent: dom,
+    })
+    return () => {
+      cmView.destroy()
+      cmView = /** @type {any} */(null)
+    }
+  }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<button on:click={() => counter++}>Counter = {counter}</button>
+<div {@attach cm}>
+</div>
